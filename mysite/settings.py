805e17d5921c3e11254793a077ad7ccf796e7cea
Nikolay <gruffbarbecue@gmail.com>
2020-05-15 09:19:20 +0300

    deploy3

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'gruffbarbecue@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
