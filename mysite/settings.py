1fed9a2afed78647771914e1aa7aa1c44102e1c8
Николай Кокорев <gruffbarbecue@gmail.com>
2020-05-15 10:41:50 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['MY_HEROKU_DOMEN=^__^=']
+ALLOWED_HOSTS = ['MY_HEROKU_DOMAIN=^__^=']
 
 
 # Application definition
